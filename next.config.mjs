7073a0dea4fdd8222a9f22453d7f7e104736f819
Fran Fares <franciscofares23@gmail.com>
2023-08-09 21:13:37 -0300

    Components config setup

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import("next").NextConfig} */
+const config = {
+    reactStrictMode: true,
+    images: {
+      domains: [
+        "www.google.com",
+        "avatar.vercel.sh",
+        "res.cloudinary.com",
+        "lh3.googleusercontent.com",
+        "pbs.twimg.com",
+      ],
+    },
+  };
+  
+  export default config;
+  
