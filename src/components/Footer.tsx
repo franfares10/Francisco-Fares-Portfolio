70a7ba0effb810262bb983f04036d414d1dbc465
Fran Fares <franciscofares23@gmail.com>
2023-08-13 11:12:05 -0300

    ThemeSelectorButton update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,6 @@ export default function Footer() {
         React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
         Framer Motion, Vercel hosting.
       </p>
-      <ModeToggle/>
     </footer>
   );
 }
--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,9 +1,9 @@
 "use client"
- 
+
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
- 
+
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -11,27 +11,19 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
- 
+
 export function ModeToggle() {
-  const { setTheme } = useTheme()
- 
+  const { setTheme, theme } = useTheme()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+        <Button variant="ghost" size="icon" className="rounded-full">
+          {theme === "dark"
+            ? <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" onClick={() => setTheme("light")} />
+            : <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" onClick={() => setTheme("dark")} />}
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
