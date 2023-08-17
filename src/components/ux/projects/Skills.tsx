492b744940dcae820551872305a12b20d1b92415
Fran Fares <franciscofares23@gmail.com>
2023-08-17 20:01:41 -0300

    ToggleButton hydration fix

---
+++ src/components/ux/projects/Skills.tsx
@@ -0,0 +1,44 @@
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
+
+interface Props {
+    technologies: string[]
+}
+
+const Skills = ({technologies}:Props) => {
+    return(
+        <ul className="flex flex-wrap py-10 items-center justify-center gap-2">
+        {technologies.map((tech, index) => (
+             <motion.li
+             className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+             key={index}
+             variants={fadeInAnimationVariants}
+             initial="initial"
+             whileInView="animate"
+             onAnimationEnd={() => index=0}
+             viewport={{
+               once: false,
+             }}
+             custom={index}
+           >
+             {tech}
+           </motion.li>
+        ))}
+    </ul>
+    );
+}
+
+export default Skills;
--- src/components/ux/projects/Swiper.tsx
+++ src/components/ux/projects/Swiper.tsx
@@ -13,6 +13,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import Skills from "./Skills";
 
 const fadeInAnimationVariants = {
   initial: {
@@ -30,7 +31,7 @@ const fadeInAnimationVariants = {
 
 const ProjectSwiper = () => {
     const [currentProject, setCurrentProject] = useState(projects[0]);
-    const [index, setIndex] = useState(1);
+
     return (
         <div className="w-1/2 h-full flex flex-col items-center justify-center pt-10">
             <Swiper
@@ -42,7 +43,7 @@ const ProjectSwiper = () => {
             >
                 {projects.map((project, index) => (
                     <SwiperSlide className="hover:scale-y-150" key={index}>
-                        <motion.div layoutId={project.id.toString()}>
+                        <motion.div>
                             {project.image
                                 ? <Image className="px-4" src={project.image} alt="p" />
                                 : <Label style={{ color: "#0084AE" }} className="text-6xl font-thin">{project.name}</Label>
@@ -51,23 +52,7 @@ const ProjectSwiper = () => {
                     </SwiperSlide>
                 ))}
             </Swiper>
-            <ul className="flex flex-wrap py-10 items-center justify-center gap-2">
-                {currentProject.technologies.map((tech, index) => (
-                     <motion.li
-                     className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
-                     key={index}
-                     variants={fadeInAnimationVariants}
-                     initial="initial"
-                     whileInView="animate"
-                     viewport={{
-                       once: true,
-                     }}
-                     custom={index}
-                   >
-                     {tech}
-                   </motion.li>
-                ))}
-            </ul>
+            <Skills technologies={currentProject.technologies}/>
         </div>
     );
 };
