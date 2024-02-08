cdd61e6978462a3f30f878cf92af3bcd84976499
Francisco Fares <francisco.fares@cognitlabs.com>
2024-02-08 08:25:26 -0300

    feat: experience updated

--- src/app/employment_history/experience.tsx
+++ src/app/employment_history/experience.tsx
@@ -1,56 +1,63 @@
-"use client";
+'use client';
 
-import React from "react";
-import SectionHeading from "../../components/ux/employment_history/section-heading";
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-import experiencesData from "@/components/data/experiences";
-import { useSectionInView } from "@/components/hooks/useSectionInView";
-import { useTheme } from "next-themes"
-import CompanyAvatar from "../../components/ux/employment_history/company-avatar";
-import { motion } from "framer-motion";
+import React from 'react';
+import SectionHeading from '../../components/ux/employment_history/section-heading';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import experiencesData from '@/components/data/experiences';
+import { useSectionInView } from '@/components/hooks/useSectionInView';
+import { useTheme } from 'next-themes';
+import CompanyAvatar from '../../components/ux/employment_history/company-avatar';
+import { motion } from 'framer-motion';
 
 export default function Experience() {
-  const { ref } = useSectionInView("Experience");
+  const { ref } = useSectionInView('Experience');
   const { theme, setTheme } = useTheme();
   return (
-    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+    <section id='experience' ref={ref} className='scroll-mt-28 mb-28 sm:mb-40'>
       <SectionHeading>My experience</SectionHeading>
 
-      <VerticalTimeline lineColor="#f3f4f6">
+      <VerticalTimeline lineColor='#f3f4f6'>
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
               visible={true}
               contentStyle={{
                 background:
-                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
-                boxShadow: "none",
-                border: "1px solid rgba(0, 0, 0, 0.05)",
-                textAlign: "left",
-                padding: "1.3rem 2rem",
+                  theme === 'light' ? '#f3f4f6' : 'rgba(255, 255, 255, 0.05)',
+                boxShadow: 'none',
+                border: '1px solid rgba(0, 0, 0, 0.05)',
+                textAlign: 'left',
+                padding: '1.3rem 2rem',
               }}
               contentArrowStyle={{
                 borderRight:
-                  theme === "light"
-                    ? "0.4rem solid #9ca3af"
-                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
+                  theme === 'light'
+                    ? '0.4rem solid #9ca3af'
+                    : '0.4rem solid rgba(255, 255, 255, 0.5)',
               }}
               date={item.date}
-              dateClassName="md:mx-5"
+              dateClassName='md:mx-5'
               icon={<CompanyAvatar company={item} />}
               iconStyle={{
-                background:
-                  theme === "light" ? "white" : "white",
-                fontSize: "1.5rem",
+                background: theme === 'light' ? 'white' : 'white',
+                fontSize: '1.5rem',
               }}
             >
-              <h3 className="font-semibold capitalize">{item.name}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <ul>
-                {item.description.map((bullet,index) =>{
-                  return <li key={index} className="!mt-1 !font-normal text-gray-700 dark:text-white/75">{bullet}</li>
-                })}
+              <h3 className='font-semibold capitalize'>{item.name}</h3>
+              <h5 className='font-normal !mt-0'>{item.location}</h5>
+              <ul className='pl-4'>
+                {item.description.map((bullet, index) => (
+                  <li
+                    key={index}
+                    className='!my-1 !font-normal text-gray-700 dark:text-white/75 text-wrap list-disc'
+                  >
+                    {bullet}
+                  </li>
+                ))}
               </ul>
             </VerticalTimelineElement>
           </React.Fragment>
--- src/components/data/experiences.ts
+++ src/components/data/experiences.ts
@@ -2,29 +2,41 @@ import exxonmobil from "@/assets/exxonmobil.png";
 import coderhouse from "@/assets/coderhouse.png";
 import uade from "@/assets/uade.png";
 import rakkau from "@/assets/rakkau.png";
+import cognitlabs from "@/assets/cognitlabs.png";
+
 const experiencesData = [
     {
       name: "Software Developer",
       location: "ExxonMobil, AR",
       description:[
-        "Microservices Development",
-        "Web Development",
-        "Applications Maintenance",
-        "Communication with clients from all over the world",
-        "Process Optimization",
+        "I´ve reduced a quarter part of team ticket queue through process automation 📈.",
+        "I gave support to client applications with thousands of users.",
+        "I’ve been part of the transformation of 2 applications, from a monolithic architecture to a microservices oriented architecture.",
+        "Communication with clients from all over the world 🌍.",
       ],
       image: exxonmobil,
       date: "2020-2022",
       url: "https://corporate.exxonmobil.com/"
     },
     {
+      name: "Systems Integration Engineer",
+      location: "Rakkau, AR",
+      description:[
+        "I´ve been involved in the development of a Peruvian bank system, particularly in the IAM team 🔐.",
+        "I led my team in a tranformation from cascade to an agile methodology.",
+        "I designed and developed the integrations between BFFs orchestrators & tenant applications such us homebanking web and mobile.",
+      ],
+      image: rakkau,
+      date: "May 2023 - Sept 2023",
+      url:"https://www.rakkau.com/"
+    },
+    {
       name: "Back-End Tutor",
       location: "Coderhouse, AR",
       description:[
-        "Project Guidance",
-        "Code Reviews",
-        "Mentoring",
-        "Courses of Back-End Development with NodeJS, Express, MongoDB, among others.",
+        "Ratings: 4,97/5 ⭐️.",
+        "+100 projects reviewed 📝.",
+        "~40 students guided 🧑‍🏫.",
       ],
       image: coderhouse,
       date: "2023 - present",
@@ -35,27 +47,26 @@ const experiencesData = [
       location: "Universidad Argentina de la Empresa, AR",
       description:[
         "Responsible for the subject: 'APIs'.",
-        "Teaching +100 students per year.",
-        "Fullstack development with NodeJS, Express, MongoDB, ReactJS, among others.",
+        "I´ve explained to +150 students how to create complex web applications with MERN & SERN stacks 🧑‍🏫.",
+        "+100 projects reviewed 📝.",
       ],
       image: uade,
       date: "2023 - present",
       url:"https://www.uade.edu.ar/"
     },
     {
-      name: "Systems Integration Engineer",
-      location: "Rakkau, AR",
+      name: "SSr Fullstack Developer",
+      location: "CognitLabs, AR",
       description:[
-        "Design and Architecture",
-        "Component Integration",
-        "Orchestrator Development",
-        "Testing",
-        "Environment Maintenance",
-        "Documentation",
+        "In charge a logistics 🚚 system development project being responsable for designing and implementing the E2E solution.",
+        "Side-participating in a blockchain 🔗 project being responsable of deployment tasks.",
+        "Training of new employees.",
+        "Ensure code quality through constanstly PRs reviews.",
+        "Taking requirements based on customer needs."
       ],
-      image: rakkau,
-      date: "2023 - present",
-      url:"https://www.rakkau.com/"
+      image: cognitlabs,
+      date: "Sept 2023 - present",
+      url:"https://cognitlabs.com/"
     },
   ];
 
